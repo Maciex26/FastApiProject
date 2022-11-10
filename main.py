090ff8391030dc7736f2ba83cb72fb9b82452d07
Maciex26 <maciex66@gmail.com>
2022-11-10 23:22:25 +0100

    test :)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse, Response
 import cv2
+import numpy as np
 
 app = FastAPI()
 
@@ -13,6 +15,7 @@ async def root():
 async def root():
     return {"MONKE"}
 
+
 @app.get("/prime/{number}")
 async def prime(number: int):
     if 1 <= number <= 9223372036854775807:
@@ -24,4 +27,16 @@ async def prime(number: int):
         return {"Input is not supported"}
 
 
-# @app.post("/picture/invert")
+@app.post("/picture/invert")
+async def invert(file: UploadFile):
+    contents = await file.read()
+    nparray = np.fromstring(contents, np.uint8)
+    # image = cv2.imread(contents, 0)
+    # inverted_image = cv2.bitwise_not(image)
+    # inverted_image = np.invert(nparray)
+    img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
+    inverted_image = cv2.bitwise_not(img)
+    cv2.imwrite("inverted.jpg", inverted_image)
+    return FileResponse("inverted.jpg")
+    # return Response(content=inverted_image, media_type="image/jpg")
+
