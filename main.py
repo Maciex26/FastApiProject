0c4c4ca6eb1f9724307438012e61326eadff2e0f
Maciex26 <maciex66@gmail.com>
2022-11-10 14:30:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import cv2
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/prime/{number}")
+async def prime(number: int):
+    if 1 <= number <= 9223372036854775807:
+        for i in range(2, int(number ** 0.5) + 1):
+            if (number % i) == 0:
+                return {f"{number} isn't prime"}
+        return {f"{number} is prime"}
+    else:
+        return {"Input is not supported"}
+
+
+# @app.post("/picture/invert")
