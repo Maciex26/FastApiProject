d1854db47cb5cd4f343dcf4927727b112b831696
Maciex26 <maciex66@gmail.com>
2022-12-17 23:04:16 +0100

    skończone :)

--- locustfile.py
+++ locustfile.py
@@ -4,15 +4,15 @@ from locust import HttpUser, task
 class PerformanceTests(HttpUser):
 
     @task
-    def prime_number(self):
+    def prime(self):
         self.client.get(url='prime/500')
 
     @task
-    def post_img(self):
+    def invert(self):
         in_file = open('pic.jpg', 'rb')
         data = in_file.read()
         self.client.post(url="picture/invert", files={'file': data})
 
     @task
-    def check_user(self):
+    def read_current_user(self):
         self.client.get(url='time', auth=("test", "test"))
